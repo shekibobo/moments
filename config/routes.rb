b5b365a443ec9935d8d0676a0b0ff2d8da177756
Joshua Kovach <kovach.jc@gmail.com>
2010-12-01 12:57:27 -0500

    fixing things

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :galleries
 
-  
-  map.login "login", :controller => "user_sessions", :action => "new"
-  map.logout "logout", :controller => "user_sessions", :action => "destroy"
-
-  map.resources :user_sessions
-  map.resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
 
